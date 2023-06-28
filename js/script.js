20c236a824c992fd85dd88b72bc6d88d2e1750ea
GiorgiSarkisyan <gios48320@gmail.com>
2023-06-28 19:54:57 +0400

    commits

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const firstName = 32;
+if (firstName === "32") {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+const secondName = 32;
+if (secondName == "32") {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
